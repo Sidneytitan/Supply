bc9485ecb6d20e0a810859e5e3dd47bff7686dbb
Sidneytitan <142373087+Sidneytitan@users.noreply.github.com>
2023-12-24 11:26:54 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,123 +1,37 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-from flask_socketio import SocketIO, emit
 from flask_pymongo import PyMongo
 from bson import ObjectId
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
-
-# Configuração do MongoDB
 app.config['MONGO_URI'] = "mongodb+srv://sidneycko:titanbetty@cluster0.feenv6t.mongodb.net/supply"
 mongo = PyMongo(app)
+socketio = SocketIO(app)
 
 # Conectar a diferentes coleções
 collection_pedido = mongo.db.pedido
 collection_cotacao = mongo.db.cotacao
 collection_lista_fornecedor = mongo.db.lista_fornecedor
 
+# Restante do código...
+
 # Rota para exibir a página inicial
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
 
-# Rota para exibir a lista de fornecedores
-@app.route('/listafornecedor')
-def lista_fornecedor():
-    # Obtendo dados do MongoDB
-    data_list = list(collection_lista_fornecedor.find())
-    return render_template('index.html', data_list=data_list)
-
-# Rota para exibir o formulário de adição
-@app.route('/adicionar', methods=['GET'])
-def exibir_formulario_adicao():
-    return render_template('adicionar.html')
-
-# Rota para lidar com a adição de fornecedores (POST)
-@app.route('/adicionar_fornecedor', methods=['POST'])
-def adicionar_fornecedor():
-    if request.method == 'POST':
-        novo_fornecedor = {
-            'fornecedor': request.form['fornecedor'],
-            'cnpj': request.form['cnpj'],
-            'telefone': request.form['telefone'],
-            'responsavel': request.form['responsavel'],
-            'local': request.form['local'],
-            'cep': request.form['cep'],
-            'Ramo': request.form['ramo'],
-            'Forma_pagamento': request.form['forma_pagamento']
-        }
-
-        # Insere o fornecedor no MongoDB
-        collection_lista_fornecedor.insert_one(novo_fornecedor)
-
-        # Emitir evento Socket.IO para atualizar Kanban em tempo real
-        socketio.emit('atualizar_kanban', namespace='/kanban')
-
-    return redirect(url_for('lista_fornecedor'))
-
-# Adicione uma rota para lidar com a pesquisa
-@app.route('/pesquisar', methods=['GET'])
-def pesquisar():
-    # Obter os parâmetros da pesquisa
-    filtro = request.args.get('filtro')
-    termo = request.args.get('termo')
-
-    # Construir a consulta MongoDB com base nos parâmetros de pesquisa
-    query = {filtro: {'$regex': termo, '$options': 'i'}}
-    data_list = list(collection_lista_fornecedor.find(query))
-
-    return render_template('index.html', data_list=data_list)
-
+# Rota para exibir a página de pedidos
 @app.route('/pedido')
 def pedido():
-    # Busca todos os pedidos no banco de dados
     dados_kanban = list(collection_pedido.find())
     return render_template('pedido.html', dados_kanban=dados_kanban)
 
-# Rota para adicionar um novo pedido
-@app.route('/add', methods=['POST'])
-def add():
-    # Obtém dados do formulário
-    titulo = request.form.get('new-task-title')
-    descricao = request.form.get('new-task-description')
-    status = request.form.get('new-task-status')
-
-    # Insere o novo pedido no banco de dados
-    new_task = {'titulo': titulo, 'descricao': descricao, 'status': status}
-    result = collection_pedido.insert_one(new_task)
-
-    # Emitir evento Socket.IO para atualizar Kanban em tempo real
-    socketio.emit('atualizar_kanban', namespace='/kanban')
-
-    # Retorna a resposta em formato JSON
-    return jsonify({'success': True, 'message': 'Pedido adicionado com sucesso!', 'taskId': str(result.inserted_id)})
-
-# Rota para excluir um pedido
-@app.route('/delete/<pedido_id>', methods=['DELETE'])
-def delete(pedido_id):
-    # Remove o pedido do banco de dados pelo ID
-    collection_pedido.delete_one({'_id': ObjectId(pedido_id)})
-
-    # Emitir evento Socket.IO para atualizar Kanban em tempo real
-    socketio.emit('atualizar_kanban', namespace='/kanban')
-
-    return jsonify({'message': 'Pedido excluído com sucesso', 'success': True})
-
-# Rota para a página Kanban
-@app.route('/kanban')
-def kanban():
-    # Obtendo dados do MongoDB
-    dados_kanban = list(collection_cotacao.find())
-    return render_template('kanban.html', dados_kanban=dados_kanban)
+# Restante do código...
 
 # Rota para obter todas as cotações
 @app.route('/dados_kanban', methods=['GET'])
 def get_dados_kanban():
-    # Obtendo dados do MongoDB
     dados_kanban = list(collection_cotacao.find())
-
-    # Converter ObjectId para str para torná-lo serializável em JSON
     dados_kanban_serializable = [
         {
             "_id": str(dado["_id"]),
@@ -127,14 +41,12 @@ def get_dados_kanban():
         }
         for dado in dados_kanban
     ]
-
     return jsonify({'dados_kanban': dados_kanban_serializable})
 
 # Rota para adicionar um novo item ao Kanban
 @app.route('/criar_cotacao', methods=['POST'])
 def criar_cotacao():
     data = request.form
-
     if 'new-task-title' in data and 'new-task-description' in data:
         novo_item = collection_cotacao.insert_one({
             'titulo': data['new-task-title'],
@@ -142,12 +54,8 @@ def criar_cotacao():
             'status': data['new-task-status']
         })
 
-        # Recupere o ID da cotação recém-criada
         novo_item_id = str(novo_item.inserted_id)
-
-        # Emitir evento Socket.IO para atualizar Kanban em tempo real
         socketio.emit('atualizar_kanban', namespace='/kanban')
-
         return jsonify({'success': True, 'message': 'Cotação criada com sucesso!', 'taskId': novo_item_id})
 
     return jsonify({'success': False, 'message': 'Falha ao criar cotação.'})
@@ -155,36 +63,23 @@ def criar_cotacao():
 # Rota para mover um item para uma nova coluna
 @app.route('/mover_item_coluna/<item_id>/<nova_coluna>', methods=['PUT'])
 def mover_item_coluna(item_id, nova_coluna):
-    # Atualizar o status do item no MongoDB
     collection_cotacao.update_one(
         {'_id': ObjectId(item_id)},
         {'$set': {'status': nova_coluna}}
     )
-
-    # Emitir evento Socket.IO para atualizar Kanban em tempo real
     socketio.emit('atualizar_kanban', namespace='/kanban')
-
     return jsonify({'message': 'Item movido com sucesso!'})
 
-# Rota para excluir uma cotação
-@app.route('/excluir_cotacao/<item_id>', methods=['DELETE'])
-def excluir_cotacao(item_id):
-    try:
-        # Excluir o item do MongoDB
-        result = collection_cotacao.delete_one({'_id': ObjectId(item_id)})
+# Restante do código...
 
-        if result.deleted_count == 1:
-            # Emitir evento Socket.IO para atualizar Kanban em tempo real
-            socketio.emit('atualizar_kanban', namespace='/kanban')
-            return jsonify({'message': 'Cotação excluída com sucesso!'})
-        else:
-            return jsonify({'error': 'Cotação não encontrada'}), 404
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+# Função para lidar com a conexão de um cliente via WebSocket
+@socketio.on('connect', namespace='/kanban')
+def handle_connect():
+    print('Client connected')
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+    socketio.run(app, debug=True)
+
 
 #Anotação
 
